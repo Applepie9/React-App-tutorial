c41b95b96632d77d2c14bf8323021fc1bd7d1f04
Applepie9 <horel.dora@gmail.com>
2021-11-27 10:18:46 +0100

    Navbar, add routes

--- app/index.js
+++ app/index.js
@@ -3,14 +3,39 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
+import { ThemeProvider } from './contexts/theme'
+import Nav from './components/Nav'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({theme}) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  }
+
   render() {
-    const name = 'Dori'
-    return (
-      <div className='container'>
-        <Battle />
-      </div>
+    return(
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className='container'>
+              <Nav />
+              <Routes>
+                <Route path='/' element={< Popular />} />
+                <Route exact path='/battle' element={< Battle />} />
+              </Routes>
+            </div>
+          </div>
+        </ThemeProvider>
+      </Router>
     )
   }
 }
